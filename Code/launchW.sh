2a425f1751ac04d3a52b2cb1a5fb49932daaaa48
Sami <sami.boulechfar@telecomnancy.eu>
2021-11-10 15:55:50 +0100

    Correction du fichier launchW.sh

--- Code/launchW.sh
+++ Code/launchW.sh
@@ -1,4 +1,8 @@
+#!/bin/bash
+echo which example \( test \) \?
+read -r varname
+echo thanks wait
 java -jar ./lib/antlr-4.9.2-complete.jar expr.g4 -no-listener -no-visitor -o ./src/parser
 javac  -cp "./lib/antlr-4.9.2-complete.jar;./src" ./src/Main.java -d ./bin
-java -cp "./lib/antlr-4.9.2-complete.jar;./bin" Main ./examples/test.exp
+java -cp "./lib/antlr-4.9.2-complete.jar;./bin" Main ./examples/$varname.exp
 
