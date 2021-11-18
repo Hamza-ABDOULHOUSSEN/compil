1cf7339a651fc6a066551d9d31a8be65abe05bb3
Sami <sami.boulechfar@telecomnancy.eu>
2021-11-18 14:21:10 +0100

    Mise a jour de la classe AstVisitor

--- Code/src/ast/AstVisitor.java
+++ Code/src/ast/AstVisitor.java
@@ -3,5 +3,7 @@ package ast;
 public interface AstVisitor<T> {
 
     public T visit(Fichier fichier);
-
+    public T visit(Decl_typ decl_typ);
+    public T visit(Decl_fct_A decl_fct_A);
+    public T visit(Decl_fct_B decl_fct_B);
 }
