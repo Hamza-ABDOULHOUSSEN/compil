b2121ea7a3d6c244bc8ad5e59c419fec74f8d27d
ABDOULHOUSSEN Hamza <hamza.abdoulhoussen@telecomnancy.eu>
2021-11-28 12:22:22 +0100

    Correction warning

--- Code/src/ast/AstVisitor.java
+++ Code/src/ast/AstVisitor.java
@@ -2,21 +2,18 @@ package ast;
 
 public interface AstVisitor<T> {
 
-    public T visit(Fichier fichier);
-    public T visit(Decl_typ decl_typ);
-    public T visit(DeclVarInt declvarint);
-    public T visit(DeclVarStruct declvarstruct);
-    public T visit(Ident ident);
-    public T visit(Int entier);
-    public T visit(ParamInt paramint);
-    public T visit(ParamStruct paramstruct);
-    public T visit(If ifinstr);
-    public T visit(IfElse ifelseinstr);
-    public T visit(While whileinstr);
-    public T visit(Return ret);
-    public T visit(Et et);
-
-
-
+     T visit(Fichier fichier);
+     T visit(Decl_typ decl_typ);
+     T visit(DeclVarInt declvarint);
+     T visit(DeclVarStruct declvarstruct);
+     T visit(Ident ident);
+     T visit(Int entier);
+     T visit(ParamInt paramint);
+     T visit(ParamStruct paramstruct);
+     T visit(If ifinstr);
+     T visit(IfElse ifelseinstr);
+     T visit(While whileinstr);
+     T visit(Return ret);
+     T visit(Et et);
 
 }
