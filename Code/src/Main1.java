936b6baa629c94693230dc99a74660b3c3c6c30c
Maha Khatib <Maha.Khatib@telecomnancy.eu>
2021-12-28 15:22:14 +0100

    modified main

--- Code/src/Main1.java
+++ Code/src/Main1.java
@@ -40,7 +40,7 @@ public class Main1 {
             JPanel panel = new JPanel();
             TreeViewer viewer = new TreeViewer(Arrays.asList(
                     parser.getRuleNames()),program);
-            viewer.setScale(1.5); // Scale a little
+            viewer.setScale(1.0); // Scale a little
             panel.add(viewer);
             frame.add(panel);
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
