c2c8574998c0723e93f8eba9af2dc9a3df7c9a86
Maha Khatib <Maha.Khatib@telecomnancy.eu>
2021-12-29 18:53:43 +0100

    gestion des blocs

--- Code/src/tds/TDSBlocEntry.java
+++ Code/src/tds/TDSBlocEntry.java
@@ -2,7 +2,24 @@ package tds;
 
 import ast.Bloc;
 
-public class TDSBlocEntry {
-    
+public class TDSBlocEntry extends TDSEntry {
+    private TdsVisitor visitor;
+
+    public TDSBlocEntry(Bloc bloc) {
+        this.visitor = new TdsVisitor();
+        for (ast.Ast vars: bloc.vars) {
+            if (vars == null) continue;
+            vars.accept(this.visitor);
+        }
+    }
+
+    public TDS getTable() {
+        return this.visitor.getTable();
+    }
+
+    public TdsVisitor getVisitor() {
+        return this.visitor;
+    }
+
     
 }
