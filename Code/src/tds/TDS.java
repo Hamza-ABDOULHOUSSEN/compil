681b18fa5dc31380c0c8b24a8d6abc3c0765a24d
Maha Khatib <Maha.Khatib@telecomnancy.eu>
2022-01-04 13:44:12 +0100

    update classe TDS

--- Code/src/tds/TDS.java
+++ Code/src/tds/TDS.java
@@ -1,19 +1,53 @@
 package tds;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.stream.Collectors;
+import org.stringtemplate.v4.compiler.CodeGenerator.conditional_return;
 
 public class TDS {
     private ArrayList<TDSEntry> entries;
+    private HashMap<String, TDSEntry> refEntries;
+    private HashMap<String, TDSStructDecl> refStructs;
     private TDS father = null;
 
     public TDS() {
         this.entries = new ArrayList<>();
+        this.refEntries = new HashMap<>();
+		this.refStructs = new HashMap<>();
     }
 
-    public int addEntry(TDSEntry entry) {
-        entries.add(entry);
-        return entries.size();
+    public void addEntry(TDSEntry entry) throws IllegalArgumentException{
+        if (entry instanceof TDSFuncEntry || entry instanceof TDSVarEntry) { 
+            TDSNamedEntry tnEntry = (TDSNamedEntry) entry;
+			if (refEntries.containsKey(tnEntry.getName())) throw new IllegalArgumentException("declaration d'une variable déjà connue !");
+			refEntries.put(tnEntry.getName(), entry);
+		}
+		entries.add(entry);
     }
+
+    public void addRefStruct(TDSStructDecl struct) {
+		this.refStructs.put(struct.name, struct);
+	}
+
+    public TDSEntry getRefEntry(String idfEntry) {
+		if (refEntries.containsKey(idfEntry)) {
+			return refEntries.get(idfEntry);
+		}
+		else if (this.father != null) {
+			return father.getRefEntry(idfEntry);
+		}
+		else return null;
+	}
+
+    public TDSStructDecl getRefStruct(String idfStruct) {
+		if (refStructs.containsKey(idfStruct)) {
+			return refStructs.get(idfStruct);
+		}
+		else if (this.father != null) {
+			return father.getRefStruct(idfStruct);
+		}
+		else return null;
+	}
 
     public TDS getFatherTDS() {
 		return this.father;
@@ -34,24 +68,17 @@ public class TDS {
         }
         String ret = strind + "TDS :";
         if (this.father == null) ret += "root\n";
-        else ret += "son of another one\n";
+        else ret += "sub\n";
         for (TDSEntry entry : entries) {
-            ret += strind + "Entry: ";
+            ret += strind + ">Entry: ";
             ret += entry.toString() + "\n";
         }
 
         for (TDSEntry entry: entries) {
-            if (entry instanceof TDSStructEntry) {
-                ret  += strind + "containing following TDS (Struct):\n";
-                TDSStructEntry sentry = (TDSStructEntry)entry;
-                ret += strind + sentry.entries.toString2(ind + 1);
-            }
-        }
-
-        for (TDSEntry entry: entries) {
             if (entry instanceof TDSIfEntry) {
-                ret += strind + "containing following TDS (IfThen/IfThenElse):\n";
                 TDSIfEntry fentry = (TDSIfEntry)entry;
+                if (fentry.thenBloc == null && fentry.elseBloc == null) continue;
+                ret += strind + "containing following TDS (IfThen/IfThenElse):\n";
                 if (fentry.thenBloc != null && fentry.thenBloc.getTable().toString() != null) {
                     ret += strind + fentry.thenBloc.getTable().toString2(ind + 1);
                 }
