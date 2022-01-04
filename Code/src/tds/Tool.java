257be84b33d1c190140b5f8fc5385714f913d9ba
Maha Khatib <Maha.Khatib@telecomnancy.eu>
2022-01-04 20:10:12 +0100

    ajout du fichier Tool

--- Code/src/tds/Tool.java
+++ Code/src/tds/Tool.java
@@ -9,6 +9,10 @@ public class Tool {
         this.tds = tds;
     }
 
+
+    //  int -> 0
+    //  struct -> 1
+    //  other -> -1
     public int typeOf (int i) {
         return 0;
     }
@@ -17,5 +21,17 @@ public class Tool {
         if (ast instanceof Bloc || ast instanceof DefStruct || ast instanceof Fichier || ast instanceof If || ast instanceof IfElse || ast instanceof Return || ast instanceof DefFctIntParam || ast instanceof ParamInt || ast instanceof DeclVarInt || ast instanceof DefFctStructParam || ast instanceof ParamStruct || ast instanceof DeclVarStruct || ast instanceof While) {
             return -1;
         }
+        if (ast instanceof Affect) {
+            return typeOf(((Affect)ast).left);
+        }
+        //if (ast instanceof ExprOper) {
+        //   return 0;
+        //}
+        Parenthese
+        if (ast instanceof Parenthese) {
+            Parenthese p = (Parenthese)ast;
+            return typeOf(p.expr);
+        }
     }
+
 }
