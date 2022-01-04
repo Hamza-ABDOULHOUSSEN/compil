515b5942cb4d3accc95fc303aa9307739691c46a
Maha Khatib <Maha.Khatib@telecomnancy.eu>
2022-01-04 18:59:42 +0100

    parcours TDS

--- Code/src/tds/TDSVisitor.java
+++ Code/src/tds/TDSVisitor.java
@@ -237,7 +237,12 @@ public class TdsVisitor implements AstVisitor<Void> {
     }
 
     @Override
-    public Void visit(Inf inf) {
+    public Void visit(InfEgal infEgal) {
+        return null;
+    }
+
+    @Override
+    public Void visit(InfEgal infEgal) {
         return null;
     }
 }
