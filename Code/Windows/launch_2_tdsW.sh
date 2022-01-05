3f4f805ceb1c7e43924bb7b73f645d2cdc2dc177
Sami Boulechfar <sami.boulechfar@telecomnancy.eu>
2022-01-05 18:16:41 +0100

    Ajout du launch_2_tdsW

---
+++ Code/Windows/launch_2_tdsW.sh
@@ -0,0 +1,85 @@
+varname=$1
+
+if [ "$varname" == "" ]; then
+
+  echo "test files :"
+  cd ./examples
+  find * | grep '[^X].exp$'
+  cd ..
+  echo
+
+  read -p "Which test from examples (ex Test_Complet/test) : " varname
+  echo
+
+fi
+
+if ! [[ $varname = *.exp ]]; then
+    varname=$varname.exp
+fi
+
+FILE=./examples/$varname
+
+if test -f "$FILE"; then
+    echo "[+] Parser creation"
+    java -jar ./lib/antlr-4.9.2-complete.jar expr.g4 -no-listener -visitor -o ./src/parser
+
+    echo "[+] Compilation"
+    javac  -cp "./lib/antlr-4.9.2-complete.jar;./src" ./src/Main3.java -d ./bin
+
+    tree=$2
+
+    if [ "$tree" == "" ]; then
+
+        read -p "Which name for the tds : " tree
+        echo
+
+    fi
+
+    if [[ $tree =~ .+/.* ]]; then
+        treename="${tree##*/}"
+        treedir="${tree%/$treename}"
+        mkdir -p "out/tds/dot/$treedir"
+        mkdir -p "out/tds/svg/$treedir"
+    fi
+
+    if test -f "./out/tds/dot/$tree.dot"; then
+        ov="y"
+
+        if [ "$2" == "" ]; then
+            echo "file already exists"
+            read -p "overwrite [y/n] : " ov
+            echo
+        fi
+
+        if [ "$ov" == "y" ]; then
+            echo "[+] TDS file dot generation"
+            java -cp "./lib/antlr-4.9.2-complete.jar;./bin" Main3 $FILE $tree >/dev/null
+
+            echo "[+] TDS file svg generation"
+            echo -Tsvg ./out/tds/dot/$tree.dot -o ./out/tds/svg/$tree.svg >/dev/null
+
+            echo "[+] Done, files are in out"
+        fi
+
+    else
+
+        echo "luffy"
+
+        echo "[+] TDS file dot generation" >/dev/null
+        java -cp "./lib/antlr-4.9.2-complete.jar;./bin" Main3 $FILE $tree >/dev/null
+
+        echo "[+] TDS file svg generation"
+        echo -Tsvg ./out/tds/dot/$tree.dot -o ./out/tds/svg/$tree.svg >/dev/null
+
+        echo "[+] Done, files are in out"
+    fi
+
+else
+    echo "ERROR"
+    echo "file : $FILE "
+    echo "does not exist"
+fi
+
+echo
+
+
