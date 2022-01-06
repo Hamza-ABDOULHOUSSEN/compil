238413d8e7d172f1ce5a835c362e6ef99d11f12c
ABDOULHOUSSEN Hamza <hamza.abdoulhoussen@telecomnancy.eu>
2022-01-07 00:44:25 +0100

    Affichage erreur

--- Code/bash/launch_2_tds.sh
+++ Code/bash/launch_2_tds.sh
@@ -22,6 +22,12 @@ fi
 FILE=./examples/$varname
 
 if test -f "$FILE"; then
+
+    rm temp1 2>/dev/null
+    rm temp2 2>/dev/null
+    touch temp1
+    touch temp2
+
     echo "[+] Parser creation"
     make parser >/dev/null
 
@@ -55,23 +61,38 @@ if test -f "$FILE"; then
 
         if [ "$ov" == "y" ]; then
             echo "[+] TDS file dot generation"
-            java -cp ./lib/antlr-4.9.2-complete.jar:./bin Main3 $FILE $tree >/dev/null
+            java -cp ./lib/antlr-4.9.2-complete.jar:./bin Main3 $FILE $tree >/dev/null 2>temp2
 
-            echo "[+] TDS file svg generation"
-            dot -Tsvg ./out/tds/dot/$tree.dot -o ./out/tds/svg/$tree.svg >/dev/null
+            if cmp -s temp1 temp2; then
+                echo "[+] TDS file svg generation"
+                dot -Tsvg ./out/tds/dot/$tree.dot -o ./out/tds/svg/$tree.svg >/dev/null
 
-            echo "[+] Done, files are in out"
+                echo "[+] Done, files are in out"
+            else
+                cat temp2
+                echo "ERROR : dot generation"
+            fi
+
         fi
 
     else
         echo "[+] TDS file dot generation"
-        java -cp ./lib/antlr-4.9.2-complete.jar:./bin Main3 $FILE $tree >/dev/null
+        java -cp ./lib/antlr-4.9.2-complete.jar:./bin Main3 $FILE $tree >/dev/null 2>temp2
 
-        echo "[+] TDS file svg generation"
-        dot -Tsvg ./out/tds/dot/$tree.dot -o ./out/tds/svg/$tree.svg >/dev/null
+        if cmp -s temp1 temp2; then
+           echo "[+] TDS file svg generation"
+           dot -Tsvg ./out/tds/dot/$tree.dot -o ./out/tds/svg/$tree.svg >/dev/null
 
-        echo "[+] Done, files are in out"
+           echo "[+] Done, files are in out"
+        else
+           cat temp2
+           echo "ERROR : dot generation"
+        fi
+
     fi
+
+    rm temp1 2>/dev/null
+    rm temp2 2>/dev/null
 
 else
     echo "ERROR"
